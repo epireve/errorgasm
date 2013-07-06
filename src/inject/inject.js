49813cfb601563615bffe01e6296a20bf17db0e9
rained23 <rained23@gmail.com>
2013-07-07 04:11:37 +0800

    Created message sender to bg and displaying SOF return result

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,7 @@
-chrome.extension.sendMessage({}, function(response) {
+/* Errorgasm - A savior to deal with Web Programming errors 		*/
+/* Detecting error of JavaScript(currently) and send Message to BG	*/
+//Dummying error event
+chrome.extension.sendMessage({ dev_code_error: "invalid character"}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
@@ -8,6 +11,20 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
+
 	}
 	}, 10);
+	$('<div id="selesaiMasalah">Logo Errorgasm 5<br><ul id="solution"></ul></div>').prependTo('body');
+	
+	for(var solution in response.errorgasm_solver.items)
+	{
+		var $res = $("<li />");
+		$res.append(response.errorgasm_solver.items[solution].title);
+		$res.appendTo($('#solution'));
+	}
+	
+	
+	 
 });
+
+
