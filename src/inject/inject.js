471fa369aba57cfb0321715aa72d3b5981cd1b23
rained23 <rained23@gmail.com>
2013-07-07 04:22:15 +0800

    removing rubbish

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,18 +2,7 @@
 /* Detecting error of JavaScript(currently) and send Message to BG	*/
 //Dummying error event
 chrome.extension.sendMessage({ dev_code_error: "invalid character"}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-
-	}
-	}, 10);
 	$('<div id="selesaiMasalah">Logo Errorgasm 5<br><ul id="solution"></ul></div>').prependTo('body');
 	
 	for(var solution in response.errorgasm_solver.items)
@@ -22,9 +11,6 @@ chrome.extension.sendMessage({ dev_code_error: "invalid character"}, function(re
 		$res.append(response.errorgasm_solver.items[solution].title);
 		$res.appendTo($('#solution'));
 	}
-	
-	
-	 
 });
 
 
