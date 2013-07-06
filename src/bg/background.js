329ed788042221617035ed23fa1b0501f90f7b2d
rained23 <rained23@gmail.com>
2013-07-07 04:16:47 +0800

    /* Errorgasm - A savior to deal with Web Programming errors */ bg to respond to content script

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,17 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
+/* Errorgasm - A savior to deal with Web Programming errors 		*/
+/* Receiving Message from Injest.js and currently call the SOF API*/
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+ function(request, sender, sendResponse) {
+ 	console.log("Receiving error data");
+ 	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.stackexchange.com//2.1/search?pagesize=5&order=desc&sort=relevance&tagged=javascript;¬tagged=jquery&site=stackoverflow", true);
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState == 4) {
+	    // JSON.parse does not evaluate the attacker's scripts.
+	    var stack_data = JSON.parse(xhr.responseText);
+	    sendResponse({errorgasm_solver:stack_data});
+	  }
+	}
+	xhr.send();
+	return true;
+});
